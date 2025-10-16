26ec95025a2e32ef27cd6dd971dc7507ee0d0008
Ilia Shipitsin <chipitsine@gmail.com>
2025-10-16 19:04:59 +0200

    test

--- docker/pg_tests/scripts/tests/config_test.sh
+++ docker/pg_tests/scripts/tests/config_test.sh
@@ -33,8 +33,13 @@ rm -rf "${PGDATA}"
 
 wal-g --config=${TMP_CONFIG} --turbo backup-fetch "${PGDATA}" LATEST
 
+echo '-=-=-=-=-=-='
+echo $PG_VERSION
+echo '=-=-=-=-=-=-'
+
 # https://www.postgresql.org/docs/current/recovery-config.html
 if awk 'BEGIN {exit !('"$PG_VERSION"' >= 12)}'; then
+   echo 'we are ==========================='
    touch "$PGDATA/recovery.signal"
    echo "restore_command = 'echo \"WAL file restoration: %f, %p\"&& /usr/bin/wal-g --config=${TMP_CONFIG} wal-fetch \"%f\" \"%p\"'" >> "${PGDATA}/postgresql.conf"
 else
